1b0ec38afd397f98ef23a7088119fb174b60e74d
Olin <olin.goog@gmail.com>
2025-02-15 20:46:51 -0500

    functiona

--- rl.py
+++ rl.py
@@ -1,43 +1,128 @@
 import random
+import copy
+import time
 
 from tqdm import tqdm
 
-class Trainer:
-    def __init__(self):
+import numpy as np
 
-        self.states = states
+np.set_printoptions(threshold=np.inf)
+states = set()
+state_transitions = dict()
+m = np.zeros((12,12))
+mapped = []
 
-        self.state_transitions = state_transitions
-    
-    def train(self, m, n=1):
-        for i in range(0, n):
-        
-            s = Sim(self.states.copy(), self.state_transitions.copy(), self.states.copy()[0]) # (..., self.state or self.initial_state)
-            m.train(s) # or s.train(m) 
+def _map(y,x):
+    states.add((y,x))
+    mapped.append((y,x))
+    state_transitions[(y,x)] = dict()
+    if x + 1 >= m.shape[0]:
+        state_transitions[(y,x)]["E"] = (y,x)
+    else:
+        state_transitions[(y,x)]["E"] = (y,x + 1)
+        if not (y,x + 1) in mapped:
+            _map(y,x+1)
+    if x - 1 < 0:
+        state_transitions[(y,x)]["W"] = (y,x)
+    else:
+        state_transitions[(y,x)]["W"] = (y,x - 1)
+        if not (y,x - 1) in mapped:
+            _map(y,x-1)
+    if y + 1 >= m.shape[0]:
+        state_transitions[(y,x)]["S"] = (y,x)
+    else:
+        state_transitions[(y,x)]["S"] = (y + 1,x)
+        if not (y+1,x) in mapped:
+            _map(y+1,x)
+    if y - 1 < 0:
+        state_transitions[(y,x)]["N"] = (y,x)
+    else:
+        state_transitions[(y,x)]["N"] = (y-1,x)
+        if not (y-1,x) in mapped:
+            _map(y-1,x)
+
+_map(0,0)
+state_transitions[(11,11)] = {"N":(11,11), "S":(11,11), "E":(11,11), "W":(11,11)}
+gamma = 0.9
+actions = {"N", "S", "E", "W"}
+rewards = {(11,10):{"E":100}, (10,11):{"S":100}, (0,0):{"S":10}}
+initial_state = (0,0)
+
+def value(state, action, policy, sim, horizon=5):
+    sim.reset()
+    state_p, reward = sim.alter(state, action)
+    _return = reward
+    returns = []
+    #returns.append(str(reward) + " * "+str(gamma) + " ** " +str(0)+ " = " + str(reward * (gamma ** 0)))
+    i = 1
+    while i < horizon:
+        action = policy.action(state_p)
+        state_p, reward = sim.alter(state_p, action)
+
+        _return += reward * (gamma ** i) # discounting
+        #returns.append(str(reward) + " * "+str(gamma) + " ** " +str(i)+ " = " + str(reward * (gamma ** i)))
+        i += 1
+    #print(returns)
+    return _return    
+
+def render(state, reward=0):
+    n = m.copy()
+    n[state] = 1
+    print(n)
+    print(str(state) + ", " + str(reward))
 
 class Sim:
-    def __init__(self, initial_state, state_transitions, rewards):
+    def __init__(self, initial_state, state_transitions, init_rewards):
         self.initial_state = initial_state
         self.state = initial_state
         self.state_transitions = state_transitions
-        self.init_rewards = rewards
-        self.rewards = rewards.copy()
+        self.init_rewards = init_rewards # preserve init rewards
+        self.rewards = copy.deepcopy(self.init_rewards)
         
-    def reset(self):
-        self.state =  self.initial_state
-        self.rewards = self.init_rewards.copy()
-        rewards = self.init_rewards.copy()
+    def reset(self, initial_state=None):
+        if initial_state:
+            self.state = initial_state
+        else:
+            self.state =  self.initial_state
+        self.rewards = copy.deepcopy(self.init_rewards)
+        
+    def visualize(self, policy, h, initial_state=None, no_reset=False):
+        if no_reset:
+            pass
+        else:
+            self.reset(initial_state)
+        render(self.state)
+        time.sleep(1.5)
+        try:
+            for j in range(0, h):
+               
+               action = policy.action(self.state)
+               self.state, reward = self.alter(self.state, action)
+               render(self.state, reward)
+               if reward == 100:
+                   render(self.state, "100 reward received.")
+                   time.sleep(3)
+                   break
+               time.sleep(1)
+        except KeyboardInterrupt:
+            return self.state
+            print("\nExiting visualization.")
            
-    def test(self, policy, n=1000, h=100):
+           
+           
+           
+    def test(self, policy, n=1000, h=100, initial_state=None):
         rewards = 0
         for i in range(0, n):
-            self.reset()
+            self.reset(initial_state)
             for j in range(0, h):
                 action = policy.action(self.state)
                 self.state, reward = self.alter(self.state, action)
                 rewards += reward
         rewards /= n
-        print(rewards)
+
+        print("Average cumulative reward over " + str(n) + " tries: " + str(rewards))
+
         
     def alter(self, state, action):
         if action:
@@ -52,7 +137,9 @@ class Sim:
             if state in self.rewards.keys():
                 if action in self.rewards[state]:
                     reward = self.rewards[state][action]
+                    #print("resetting " + str(self.rewards[state][action]))
                     self.rewards[state][action] = 0 # So rewards can not be 'farmed'
+                    #print(self.init_rewards[state][action])
                 else:
                     reward = 0
             else:
@@ -63,25 +150,6 @@ class Sim:
             reward = 0
             
         return new_state, reward
-        
-
-
-class Model:
-    def __init__(self, policy):
-        self.returns = dict()
-        self.policies = [policy]
-        self.rewards = None
-        
-    def step(self, sim):
-        sim.alter(sim.state, action)
-        
-
-    def iterate_with(self, sim):
-        state = self.initial_state
-        for j in range(0, self.horizon):
-            action = self.pick_action(state)
-            
-            self.step(action, sim)
 
 class Policy:
     def __init__(self, states, actions, preset=None, random=False):
@@ -105,41 +173,10 @@ class Policy:
     
     def from_QTable_greedy(self, table):
         # Assuming table - policy/action agreement
-        for state in self.states:
+        for state in tqdm(self.states):
             self.action_selections[state] = max(table.table[state], key=table.table[state].get)
-        
-"""
-class State:
-    def __init__(self):
-        self.content = ""
 
-    def value(self, policy, sim):
-        if not sim:
-            sim = Sim(alter, initial_state, state_transitions, rewards)
-        i = 1
-        _return = 0
-        while i < 5:
-            action = policy.action(state)
-            __, reward = sim.alter(action)
-            _return += reward * (gamma ** i) # discounting
-            i += 1
-        return _return
-"""
 
-def value(state, action, policy, sim, horizon=5):
-    sim.reset()
-    
-    
-    action = policy.action(state)
-    state_p, reward = sim.alter(state, action)
-    _return = reward
-    i = 1
-    while i < horizon:
-        action = policy.action(state_p)
-        state_p, reward = sim.alter(state_p, action)
-        _return += reward * (gamma ** i) # discounting
-        i += 1
-    return _return    
 
 class QTable:
     def __init__(self, states):
@@ -177,89 +214,20 @@ class QTable:
                 self.table[state] = dict()
             for action in average_returns.keys():
                 self.table[state][action] = average_returns[action]
-            #print(str(state) + " with action " + str(action) + " has " + str(average_returns[action]))
-            #print(action_cnts)
-            
-# "environment" variables
-"""
-states = set()
-actions = set()
-state_transitions = {states:{actions:states}} # S x A x S -- currently not python
-rewards = {}
-initial_state = "..."
-"""               
-"""
-gamma = .5
-states = {"S", "1", "2", "3", "4", "G"}
-actions = {"N", "S", "E", "W"}
-policy_choices = {"S":"N", "2":"N", "1":"E", "3":"E", "4":"E"}
-state_transitions = {"S":{"N":"2", "S":"S"}, "2":{"N":"1"}, "1":{"E":"3", "S":"S"}, "3":{"E":"4"}, "4":{"E":"G"}, "G":{"E":"G"}} # S x A x S -- currently not python
-rewards = {"4":{"E":100}, "1":{"S":10}}
-initial_state = "1"
-"""
 
-import numpy as np
-np.set_printoptions(threshold=np.inf)
-states = set()
-state_transitions = dict()
-m = np.zeros((12,12))
-mapped = []
-
-def _map(y,x):
-    states.add((y,x))
-    mapped.append((y,x))
-    state_transitions[(y,x)] = dict()
-    if x + 1 >= m.shape[0]:
-        state_transitions[(y,x)]["E"] = (y,x)
-    else:
-        state_transitions[(y,x)]["E"] = (y,x + 1)
-        if not (y,x + 1) in mapped:
-            _map(y,x+1)
-    if x - 1 < 0:
-        state_transitions[(y,x)]["W"] = (y,x)
-    else:
-        state_transitions[(y,x)]["W"] = (y,x - 1)
-        if not (y,x - 1) in mapped:
-            _map(y,x-1)
-    if y + 1 >= m.shape[0]:
-        state_transitions[(y,x)]["S"] = (y,x)
-    else:
-        state_transitions[(y,x)]["S"] = (y + 1,x)
-        if not (y+1,x) in mapped:
-            _map(y+1,x)
-    if y - 1 < 0:
-        state_transitions[(y,x)]["N"] = (y,x)
-    else:
-        state_transitions[(y,x)]["N"] = (y-1,x)
-        if not (y-1,x) in mapped:
-            _map(y-1,x)
-
-gamma = 0.9
-actions = {"N", "S", "E", "W"}
-rewards = {(11,10):{"E":100}, (10,11):{"S":100}, (0,0):{"S":10}}
-initial_state = (0,0)
-
-
-    
-
-# should take a policy, an attribute of a model
-def run(model, sim, episode_length=10):
-    state = initial_state
-    for step in range(0, episode_length):
-        action = random.choice(list(model.policies[0].actions))
-        state, __ = sim.alter(state, action)
-        # why we doing this?
-
-def teleop():
+def teleop(fallback_policy=None):
     s = Sim(initial_state, state_transitions, rewards.copy())
     state = s.initial_state
+    render(state, "...")
     while True:
         action = input("> ")
-        state, reward = s.alter(state, action)
-        n = m.copy()
-        n[state] = 1
-        print(n)
-        print(str(state) + ", " + str(reward))
+        if action == "":
+            if not fallback_policy: # last minute
+                fallback_policy = olicy(states, actions, None, True)
+            state = s.visualize(fallback_policy, 100, state)
+        else:
+            state, reward = s.alter(state, action)
+        render(state, reward)
         # Could collect data in this time, for human_policy
         
 # for all states:
@@ -276,30 +244,6 @@ def teleop():
 # now improve MDP
 #
 #
-# Then, states are bins of continuous space
-s = Sim(initial_state, state_transitions, rewards.copy())
-policy = Policy(states, actions, preset=None, random=True)
-s.test(policy)
-qtable = QTable(states)
-qtable.train(policy, 36, s, 10) # Qtable now has returns from random walks from all states
-new_policy = Policy(states, actions, preset=None, random=False)
-new_policy.from_QTable_greedy(qtable.table) # policy now has valuable actions
-s.test(new_policy)
+# Then, states are bins of continuous space 
 
 
-if __name__ == "__main__":
-    policy = Policy(states, actions, policy_choices)
-    m = Model(policy)
-    num_iterations = 1
-    #m.iterate_with(sim)
-    for i in range(0, num_iterations):
-        sim = Sim(initial_state, state_transitions, rewards)
-        run(m, sim)
-        
-"""
-Add two policies, random and greedy.
-"""
-
-    
-    
-    
---
+++ rl_interface.py
@@ -0,0 +1,48 @@
+import rl
+
+def main():
+    rp = rl.Policy(rl.states, rl.actions, None, True)
+
+    qt = rl.QTable(rl.states)
+
+    s = rl.Sim(rl.initial_state, rl.state_transitions, rl.rewards)
+
+    qt.train(rp, 1000, s, 100)
+
+    p = rl.Policy(rl.states, rl.actions)
+
+    p.from_QTable_greedy(qt)
+
+
+    #s.visualize(p, 100, (0,0)) # stuck in (0,0)-(1,0) loop
+    #s.visualize(p, 100, (1,1)) #same
+    #s.visualize(p, 100, (2,2)) #same
+    #s.visualize(p, 100, (3,3))
+    #s.visualize(p, 100, (4,4))
+    s.visualize(p, 100, (5,5))
+    
+    
+    s.test(p, 100, 100, (0,0))
+    s.test(p, 100, 100, (5,5))
+    
+    
+    
+    s.test(rp, 100, 100, (5,5))
+    
+    s.test(p, 100, 100, (5,5))
+    
+    s.test(p, 100, 100, (10,10))
+    
+class TrainedPolicy:
+    def __init__(self):
+        rp = rl.Policy(rl.states, rl.actions, None, True)
+
+        qt = rl.QTable(rl.states)
+
+        s = rl.Sim(rl.initial_state, rl.state_transitions, rl.rewards)
+
+        qt.train(rp, 1000, s, 100)
+
+        self.policy = rl.Policy(rl.states, rl.actions)
+
+        self.policy.from_QTable_greedy(qt)
