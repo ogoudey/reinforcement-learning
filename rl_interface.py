9bb23742b31b8bee9826368e7c21a4d6b426999a
Olin <olin.goog@gmail.com>
2025-02-20 16:31:50 -0500

    converges

--- rl_interface.py
+++ rl_interface.py
@@ -5,12 +5,11 @@ def main():
     rp = rl.Policy(rl.states, rl.actions, None, True)
     
     s = rl.Sim(rl.initial_state, rl.state_transitions, rl.rewards)
+    p = rl.Policy(rl.states, rl.actions, None, True)
+    table = rl.rollout_monte_carlo(rp, initial_state=(0, 0), episode_length=100, horizon=100, rollouts=10000)
     
-    new_policy = rl.monte_carlo(rp, initial_state=(10,10), episode_length=100, horizon=100, cycles=1)
+    # table = rl.rollout_monte_carlo(rp, initial_state=(0, 0), episode_length=100, horizon=100, rollouts=10000)
     
-    return s, new_policy
+    p.from_table_greedy(table)
     
-    
-
-
-
+    return s, p
